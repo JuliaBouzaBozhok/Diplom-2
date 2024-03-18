5e81003ddd64e77fe52f3680a4abcc978fe596a3
Julia Bouza <julia.jmpr@yandex.ru>
2024-03-18 21:04:17 +0300

    added allure report to the right folder

---
+++ src/main/java/entity/Order.java
@@ -0,0 +1,21 @@
+package entity;
+
+import java.util.List;
+import java.util.ArrayList;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class Order {
+
+    private List<String> ingredients;
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public Order() {
+        ingredients = new ArrayList<>();
+    }
+}
